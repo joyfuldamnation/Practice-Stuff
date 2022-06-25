3d880004707269d192e1bc9e87bdfb1ca3071039
Akshat Pandya <akshatpandya01@gmail.com>
2022-06-25 19:53:17 +0530

    ujjwal Home practice

---
+++ Java Problems/LinkedListQues/LinkedListQ7.java
@@ -0,0 +1,36 @@
+//Merge two sorted linked lists
+
+
+/*
+  Merge two linked lists
+  head pointer input could be NULL as well for empty list
+  Node is defined as
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+*/
+
+class LinkedList
+{
+    //Function to merge two sorted linked list.
+    Node sortedMerge(Node head1, Node head2) {
+        // This is a "method-only" submission.
+        // You only need to complete this method
+        if(head1 == null) return head2;
+        if(head2 == null) return head1;
+
+        if(head1.data < head2.data)
+        {
+            head1.next = SortedMerge(head1.next, head2);
+            return head1;
+        }
+        else
+        {
+            head2.next = SortedMerge(head1, head2.next);
+            return head2;
+        }
+    }
+}
